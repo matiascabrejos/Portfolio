63193b88e4d5996b9cff6b41b8506bb0bf7416ac
matiascabrejos <matias.cabrejos@hotmail.com>
2022-01-06 23:23:02 -0300

    Working on the about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="text-white">
+  <div class="text-white wrapper">
     <div class="intro">
-      <h1 class="py-4 text-base font-semibold">Hi there!</h1>
+      <h1 class="text-title">Hi there!</h1>
       <p>
         I'm Matías Cabrejos, a Front-End Developer who is passionate about
         programming and especially of VueJS.
       </p>
-      <p>I’m always curious about learning new skills, tools, and concepts.</p>
+      <p>I’m always curious about new skills, tools, and concepts.</p>
       <p>
         I'm constantly learning about technologies, scrum and improving my soft
         skills
       </p>
       <p>
-        Apart from my coding skills i've learned about soft skills or Scrum.
-      </p>
-      <p>
-        Besides coding, I like playing football and running. I'm also really
-        into the crypto world and economics.
+        Besides coding, I enjoy playing football and running. I'm also really
+        interested into the crypto world and economics.
       </p>
       <p>
         In my free time when i'm not coding, I enjoy playing football and
@@ -29,15 +26,37 @@
       <p>Languages: English, Spanish</p>
     </div>
     <div class="what">
-      <h1 class="py-4 text-base font-semibold">What I Do Well</h1>
+      <h1 class="text-title">What I Do Well</h1>
       <p>
         Tech Stack: HTML, CSS, Sass, Tailwind CSS, JavaScript, VueJS, NuxtJS
       </p>
       <p>Deployment: Firebase, Netlify, Heroku</p>
     </div>
     <div class="touch">
-      <h1 class="py-4 text-base font-semibold">Get In Touch</h1>
+      <h1 class="text-title">Get In Touch</h1>
       <p>Feel free to:</p>
+    </div>
+    <div>
+      <h1 class="text-title">What's Next?</h1>
+      <p>
+        Every day I code in order to get better about the skills I currently
+        have.
+      </p>
+      <p>
+        Right now I'm learning about Back-End using technologies as NodeJS,
+        ExpressJS. As well as databases as SQL, MongoDB.
+      </p>
     </div>
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  @apply max-w-screen-xl mx-auto;
+}
+
+.text-title {
+  @apply py-4 text-base font-semibold;
+  color: #17b5cb;
+}
+</style>
