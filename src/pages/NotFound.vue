f431458cb442375fab946ff072b20c21da58d9f1
matiascabrejos <matias.cabrejos@hotmail.com>
2022-02-11 20:30:17 -0300

    Working in the not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1 class="text-white text-center">Page not found - Go to <router-link to="/" class="semibold">Home</router-link></h1>
+  <h1 class="text-white text-center">
+    Page not found - Go to
+    <router-link to="/" class="link">Home</router-link> or
+    <router-link to="/about" class="link">About</router-link>
+  </h1>
 </template>
+
+<style scoped>
+.link {
+  @apply font-semibold hover:underline;
+  color: #17b5cb;
+}
+</style>
