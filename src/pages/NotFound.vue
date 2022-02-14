3876c705f6692bdd782ae2847a55f6b7e0b9af59
matiascabrejos <matias.cabrejos@hotmail.com>
2022-02-13 22:14:12 -0300

    Working in the not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,14 +1,27 @@
 <template>
-  <h1 class="text-center py-4 mr-2 text-base font-semibold text-white">
-    Page not found - Go to
-    <router-link to="/" class="link">Home</router-link> or
-    <router-link to="/about" class="link">About</router-link>
-  </h1>
+  <div class="text-center">
+    <div class="border-wrapper">
+      <h1 class="text-not-found">
+        Page not found - Go to
+        <router-link to="/" class="link">Home</router-link> or
+        <router-link to="/about" class="link">About</router-link>
+      </h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .link {
   @apply font-semibold hover:underline;
   color: #17b5cb;
+}
+
+.border-wrapper {
+  @apply border-2 inline-block m-2;
+  border-color: #17b5cb;
+}
+
+.text-not-found {
+  @apply text-center text-base font-semibold text-white m-4;
 }
 </style>
