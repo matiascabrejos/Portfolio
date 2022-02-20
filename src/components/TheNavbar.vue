34d435114ce072a86d21bf1aeb62541a0815d837
matiascabrejos <matias.cabrejos@hotmail.com>
2022-02-20 15:41:45 -0300

    Changing router link of the navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -11,7 +11,7 @@
         </div>
 
         <div class="navbar hidden md:block">
-          <router-link class="nav-link" to="/">Portfolio</router-link>
+          <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="about">About</router-link>
           <router-link class="nav-link" to="back">Back</router-link>
         </div>
