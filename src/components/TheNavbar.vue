71338ea55d577b283608cd834e9b21a7de251473
matiascabrejos <matias.cabrejos@hotmail.com>
2022-02-21 23:45:47 -0300

    Changing router links of the navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -13,7 +13,7 @@
         <div class="navbar hidden md:block">
           <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="about">About</router-link>
-          <router-link class="nav-link" to="back">Back</router-link>
+          <router-link class="nav-link" to="back">Not Found</router-link>
         </div>
 
         <div class="ml-3 flex md:hidden">
@@ -55,14 +55,14 @@
     </nav>
     <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
       <div class="px-2 pt-2 pb-3 sm:px-3">
-        <router-link to="portfolio" class="mobile-link focus:outline-none"
-          >Projects
-        </router-link>
-        <router-link to="about" class="mt-1 mobile-link focus:outline-none"
-          >Blog
+        <router-link to="/" class="mobile-link focus:outline-none"
+          >Home
         </router-link>
         <router-link to="about" class="mt-1 mobile-link focus:outline-none"
           >About
+        </router-link>
+        <router-link to="back" class="mt-1 mobile-link focus:outline-none"
+          >Not Found
         </router-link>
       </div>
     </div>
