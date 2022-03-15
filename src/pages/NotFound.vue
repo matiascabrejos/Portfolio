d39d3db8606d9c989ee1b47c1ad74076694da4d8
matiascabrejos <matias.cabrejos@hotmail.com>
2022-03-14 23:36:28 -0300

    Working in the not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -9,6 +9,7 @@
       <h1>Not Found</h1>
       <h1>Page Not Found</h1>
       <h1>Woops! Something went wrong - Page not found</h1>
+      <h1>Maybe go to Home or About</h1>
     </div>
   </div>
 </template>
